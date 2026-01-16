46efebb319b4a7c02b20bb959e4f60601ab4199b
snehamusale4444 <snehamusale08@gmail.com>
2026-01-16 08:37:55 +0530

    Create Day01_helloworld.C

---
+++ Day01_helloworld.C
@@ -0,0 +1,11 @@
+#include <stdlio.h>
+
+int main (){
+    char s[100];
+    printf("Hello, World!\n");
+    scanf("%[^\n]", s);
+    printf("%s", s);
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
