8048ca2a5d9f1bdf62a6902c54e4758053629266
snehamusale4444 <snehamusale08@gmail.com>
2026-01-17 21:38:56 +0530

    Add program to calculate sum and difference of numbers

---
+++ Day02_sum_of_num.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+	int a , b;
+    float c,d;
+    a=10,b=4;
+    c =4.0,d=2.0;
+    printf("%d\t%d",a+b,a-b);
+printf("\n%.1f\t%.1f",c+d,c-d);
+    
+    
+    return 0;
+}
