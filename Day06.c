374f096a4d9cc2e62ee0a0b01b8a83ebc11b9f3b
snehamusale4444 <snehamusale08@gmail.com>
2026-01-21 22:21:38 +0530

    Add program to count digit frequencies in a string

---
+++ Day06.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    char s[1000];
+    int freq[10] = {0};
+
+    scanf("%s", s);
+
+    for (int i = 0; s[i] != '\0'; i++) {
+        if (s[i] >= '0' && s[i] <= '9') {
+            freq[s[i] - '0']++;
+        }
+    }
+
+    for (int i = 0; i < 10; i++) {
+        printf("%d ", freq[i]);
+    }
+
+    return 0;
+}
